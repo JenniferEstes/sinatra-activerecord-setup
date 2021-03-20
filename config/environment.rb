99ba74815173fa56efe1c2611b44cd01173fb162
Jennifer <jenndestes@gmail.com>
2021-03-19 19:09:50 -0500

    commit tree c5200f966e05f93c83f59dadd5b066f2857c5f8f

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,9 @@
-ENV['SINATRA_ENV'] ||= "development"
+ENV['SINATRA_ENV'] ||= "development" #telling Sinatra it should use "development" environment for both shotgun and testing suite
 
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
+configure :development do
+    set :database, 'sqlite3:db/database.db'
+end
 
 require './app'
